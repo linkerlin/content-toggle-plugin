dd19aecdb2fdeb941dfa6b8de50083c3ae7d7770
Halo Master <linker.lin@live.com>
2024-11-03 15:20:33 +0800

    修复 吃掉 标题的问题

--- content-toggle-plugin.php
+++ content-toggle-plugin.php
@@ -286,8 +286,8 @@ add_action( 'wp_enqueue_scripts', 'ctp_enqueue_scripts' );
 
 // 处理文章内容
 function ctp_process_content($content) {
-    // 修改分割方式，使用 PREG_SPLIT_DELIM_CAPTURE 标志来保留分隔符
-    $questions = preg_split('/<h[1-6][^>]*>.*?<\/h[1-6]>/i', $content, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+    // 移除 PREG_SPLIT_DELIM_CAPTURE 标志
+    $questions = preg_split('/<h[1-6][^>]*>.*?<\/h[1-6]>/i', $content, -1);
     
     foreach($questions as &$question) {
         // 处理选项,添加可点击效果
